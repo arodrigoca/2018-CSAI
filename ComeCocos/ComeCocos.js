467e133800520a1462a0eebf0b4f995a504ff601
Adrian Rodrigo Castillo <arodrigo@beta05.aulas.gsyc.urjc.es>
2018-03-22 10:54:37 +0100

    started map construction

--- ComeCocos/ComeCocos.js
+++ ComeCocos/ComeCocos.js
@@ -2,6 +2,14 @@
 function GameArea(){
 
     this.canvas = document.getElementById("canvas2D");
+    this.canvas.width = 600;
+    this.canvas.height = 600;
+    this.marginLeft = 6;
+    this.marginRight = 13;
+    this.marginTop = 6;
+    this.marginBot = 12;
+    this.lineWidth = 6;
+
     this.start = function(){
         this.ctx = this.canvas.getContext('2d');
     }
@@ -14,13 +22,31 @@ function GameArea(){
     this.buildWalls = function(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.fillStyle = 'blue';
-        this.ctx.beginPath();
-        this.ctx.moveTo(0, 0);
-        this.ctx.lineTo(100, 200);
-        this.ctx.lineTo(150, 120);
-        this.ctx.closePath();
-        this.ctx.fill();
-        this.ctx.restore();
+        //---------outer walls-----------------//
+        this.ctx.fillRect(this.marginLeft, this.marginTop, 600-this.marginRight, this.lineWidth);
+        this.ctx.fillRect(this.marginLeft, 600-this.marginBot, 600-this.marginRight, this.lineWidth);
+
+        //-----side openings in the outer walls-----//
+
+        //left side
+        this.ctx.fillRect(this.marginLeft, this.marginTop, this.lineWidth, 260);
+        this.ctx.fillRect(this.marginLeft, 340, this.lineWidth, 260-this.marginBot);
+
+        //right side
+        this.ctx.fillRect(600-this.marginRight, this.marginTop, this.lineWidth, 260);
+        this.ctx.fillRect(600-this.marginRight, 340, this.lineWidth, 260-this.marginBot);
+
+        //-----square blocks----------// corridors are 90 pixels size, margin with walls is 50 (from canvas)
+
+        this.ctx.fillRect(50, 50, 40, 40);
+        this.ctx.fillRect(140, 50, 40, 40);
+        this.ctx.fillRect(140, 140, 40, 40);
+        this.ctx.fillRect(50, 140, 40, 40);
+
+        //-------------------------------
+
+        
+
     }
 }
 
@@ -32,7 +58,7 @@ function gameObject(name, Area){
     this.ctx = Area.ctx;
     this.draw = function(){
         this.ctx.fillStyle = 'blue';
-        this.ctx.fillRect(0,0,150,100)
+        this.ctx.fillRect(0,0,150,100);
     }
 }
 
