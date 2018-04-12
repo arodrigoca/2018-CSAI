b50bbcd9f03b3cc1b9afcf702412bccda9f3026e
Adrian Rodrigo Castillo <arodrigo@beta05.aulas.gsyc.urjc.es>
2018-04-12 10:52:46 +0200

    more work done

--- ComeCocos/ComeCocos.js
+++ ComeCocos/ComeCocos.js
@@ -1,7 +1,7 @@
 
-function GameArea(){
+function GameArea(ctx, canvas){
 
-    this.canvas = document.getElementById("canvas2D");
+    this.canvas = canvas;
     this.canvas.width = 600;
     this.canvas.height = 600;
     this.marginLeft = 6;
@@ -9,10 +9,11 @@ function GameArea(){
     this.marginTop = 6;
     this.marginBot = 12;
     this.lineWidth = 6;
+    this.ctx = ctx;
 
-    this.start = function(){
-        this.ctx = this.canvas.getContext('2d');
-    }
+    //this.start = function(){
+    //    this.ctx = this.canvas.getContext('2d');
+    //}
     this.clearCanvas = function(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.fillStyle = 'black';
@@ -70,22 +71,73 @@ function GameArea(){
     }
 }
 
-function gameObject(name, Area){
-    this.x = 0;
-    this.y = 0;
-    this.width = 0;
-    this.height = 0;
-    this.ctx = Area.ctx;
-    this.draw = function(){
-        this.ctx.fillStyle = 'blue';
-        this.ctx.fillRect(0,0,150,100);
+function gameObject(id, x, y, img, ctx, canvas){
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.speed = 0; //v
+    this.angle = 0; //theta
+    this.moveAngle = 0; //w
+    var d = new Date();
+    this.tm = d.getTime();
+    this.width = function(){
+      var drawing = new Image();
+      drawing.src = img;
+      return drawing.width;
     }
+
+    this.height = function(){
+      var drawing = new Image();
+      drawing.src = img;
+      return drawing.height;
+    }
+
+    this.draw = function(){  //Draw Function
+      //ctx = canvas.getContext('2d');
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      //ctx.rotate(this.angle);
+      var drawing = new Image();
+      drawing.src = img;
+      ctx.drawImage(drawing, -drawing.width/2,-drawing.height/2);
+      ctx.restore();
+    }
+    this.update = function() { //Update. Modify X, Y and move angle depending on time
+        var d = new Date();
+        var now = d.getTime();
+        var dt = now - this.tm;
+        this.tm = now;
+        this.angle = this.angle + (this.moveAngle*(dt/1000.0));
+        this.x = this.x + this.speed * Math.sin(this.angle)*(dt/1000.0);
+        this.y = this.y - this.speed * Math.cos(this.angle)*(dt/1000.0);
+    }
+}
+
+function keyHandler(event, thing) { //Keyboard press detector
+
+  if(event.key == "ArrowLeft"){thing.x = thing.x-1;}
+  if(event.key == "ArrowRight"){thing.x = thing.x+1;}
+  if(event.key == "ArrowUp"){thing.y = thing.y-1;}
+  if(event.key == "ArrowDown"){thing.y = thing.y+1;}
+  thing.update();
+  thing.draw();
+}
+
+function render(){
+    ;
 }
 
 function startGame() {
 
-    var myGameArea = new GameArea();
-    myGameArea.start();
+    var canvas = document.getElementById("canvas2D");
+    var ctx = canvas.getContext('2d');
+    var myGameArea = new GameArea(ctx, canvas);
     myGameArea.buildWalls();
-    var thing = new gameObject('thing', myGameArea);
+    var thing = new gameObject('thing', 225, 225, 'pacman.png', ctx, canvas);
+    thing.update();
+    thing.draw();
+    document.addEventListener('keydown', function(e){
+        keyHandler(e, thing);
+    }, false);
+    renderInterval = setInterval(render, 16);
 }
