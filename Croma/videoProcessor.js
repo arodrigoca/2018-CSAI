e9a2fd295aa0132d0a64d863a98e44c166fad94d
adrian <adri_rodri_64@hotmail.es>
2018-05-13 01:22:15 +0200

    more work done

--- Croma/videoProcessor.js
+++ Croma/videoProcessor.js
@@ -30,7 +30,9 @@ function changeButtonState(event){
     buttonState = 2;
   }else if (event.target.id == 'blue'){
       buttonState = 3;
-    }
+  }else if (event.target.id == 'chroma'){
+    buttonState = 4;
+  }
 }
 
 function draw(v,c,bc,w,h) {
@@ -54,35 +56,70 @@ function draw(v,c,bc,w,h) {
 
     }else if(buttonState == 1){
       //red component
+      console.log('Only Red Component');
       for(var i = 0; i < data.length; i+=4) {
           var r = data[i];
           var g = data[i+1];
           var b = data[i+2];
-          var color = (0*r+1*g+0*b);
-          data[i] = color;
-          data[i+1] = color;
-          data[i+2] = color;
+          var rNew = r*3;
+          var gNew = 0;
+          var bNew = 0;
+          var brightness = (rNew+gNew+bNew)>>>3;
+          data[i] = brightness;
+          data[i+1] = brightness;
+          data[i+2] = brightness;
       }
     }else if(buttonState == 2){
+      //green component
+      console.log('Only Green Component');
       for(var i = 0; i < data.length; i+=4) {
+          var contrast = 100;
           var r = data[i];
           var g = data[i+1];
           var b = data[i+2];
-          var brightness = (3*r+4*g+b)>>>3;
+          var rNew = 0;
+          var gNew = g*3;
+          var bNew = 0;
+          var brightness = (rNew+gNew+bNew)>>>3;
           data[i] = brightness;
           data[i+1] = brightness;
           data[i+2] = brightness;
       }
 
     }else if(buttonState == 3){
+      console.log('Only Blue Component');
       for(var i = 0; i < data.length; i+=4) {
+          var contrast = 100;
           var r = data[i];
           var g = data[i+1];
           var b = data[i+2];
-          var brightness = (3*r+4*g+b)>>>3;
+          var rNew = 0;
+          var gNew = 0;
+          var bNew = b*3;
+          var brightness = (rNew+gNew+bNew)>>>3;
           data[i] = brightness;
           data[i+1] = brightness;
           data[i+2] = brightness;
+      }
+
+    }else if(buttonState == 4){
+      //chroma effect
+
+      console.log('Chroma effect');
+      var image = new Image();
+      image.src = 'image.jpg'
+      bc.drawImage(image, 0, 0, 600, 300);
+      var backData = bc.getImageData(0,0,w,h);
+      var dataBackData = backData.data;
+      for(var i = 0; i < data.length; i+=4) {
+        var r = data[i];
+        var g = data[i+1];
+        var b = data[i+2];
+        if (g > 100 && r < 80 && b < 80){
+          data[i] = dataBackData[i];
+          data[i+1] = dataBackData[i+1];
+          data[i+2] = dataBackData[i+2];
+        }
       }
     }
 
