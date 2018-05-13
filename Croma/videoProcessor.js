f2ff81ff940cd6f2b52e4a7a4a46bec145033f39
adrian <adri_rodri_64@hotmail.es>
2018-05-13 13:38:56 +0200

    more work done

--- Croma/videoProcessor.js
+++ Croma/videoProcessor.js
@@ -1,4 +1,4 @@
-var buttonState = 0;
+var buttonState = 6;
 
 document.addEventListener('DOMContentLoaded', function(){
     var v = document.getElementById('v');
@@ -32,11 +32,33 @@ function changeButtonState(event){
       buttonState = 3;
   }else if (event.target.id == 'chroma'){
     buttonState = 4;
+  }else if (event.target.id == 'hsi'){
+    buttonState = 5;
+  }else if (event.target.id == 'org'){
+    buttonState = 6;
   }
+}
+
+function storage(){
+
+  var redValue = document.getElementById('redComp').value;
+  var grenValue = document.getElementById('greenComp').value;
+  var blueValue = document.getElementById('blueComp').value;
+  var factorObj = {'redComp':redValue, 'greenComp':grenValue, 'blueComp':blueValue};
+  localStorage.setItem('userStorage', JSON.stringify(factorObj));
+}
+
+function resetFactor(){
+  var redValue = 1;
+  var grenValue = 1;
+  var blueValue = 1;
+  var factorObj = {'redComp':redValue, 'greenComp':grenValue, 'blueComp':blueValue};
+  localStorage.setItem('userStorage', JSON.stringify(factorObj));
 }
 
 function draw(v,c,bc,w,h) {
     if(v.paused || v.ended) return false;
+    //get the factor value from local storage
     // First, draw it into the backing canvas
     bc.drawImage(v,0,0,w,h);
     // Grab the pixel data from the backing canvas
@@ -120,6 +142,50 @@ function draw(v,c,bc,w,h) {
           data[i+1] = dataBackData[i+1];
           data[i+2] = dataBackData[i+2];
         }
+      }
+
+    }else if(buttonState == 5){
+      console.log('HSI space');
+      for(var i = 0; i < data.length; i+=4) {
+        var R = data[i];
+        var G = data[i+1];
+        var B = data[i+2];
+        var hue;
+        var sat;
+        var int;
+        var r = R/255;
+        var g = G/255;
+        var b = B/255;
+        var int = (r + g + b)/3;
+
+        if (R == G && G == B){
+    			sat = hue = 0;
+    		}else{
+          var wAngle = (r - g + r - b) / Math.sqrt((r - g) * (r - g) + (r - b) * (g - b)) / 2;
+          hue = Math.acos(wAngle) * 180 / Math.PI;
+          if(b > g){
+            hue = 360 - hue;
+          }
+          sat = 1 - Math.min(r, g, b) / int;
+        }
+        data[i] = hue;
+        data[i+1] = sat;
+        data[i+2] = int;
+      }
+
+    }else if(buttonState == 6){
+      //get user color factor
+      var userFactor = JSON.parse(localStorage.getItem('userStorage'));
+      for(var i = 0; i < data.length; i+=4) {
+          var r = data[i];
+          var g = data[i+1];
+          var b = data[i+2];
+          r = r*userFactor.redComp;
+          g = g*userFactor.greenComp;
+          b = b*userFactor.blueComp;
+          data[i] = r;
+          data[i+1] = g;
+          data[i+2] = b;
       }
     }
 
